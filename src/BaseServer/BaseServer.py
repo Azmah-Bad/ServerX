eb6b15ca9d82b3392d6c8b89297c5e4ea7f78fad
Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>
2020-12-06 01:25:20 +0100

    🐞 added remote debugger - seperated client and server addresses - timeout are now handled by socket rather than func_timeout - writeLogs - checkFile check if files are identical after client receives it

--- src/BaseServer/BaseServer.py
+++ src/BaseServer/BaseServer.py
@@ -3,7 +3,7 @@ import os
 import random
 import socket
 import time
-
+import sys
 from func_timeout import func_timeout
 
 HOST = "127.0.0.2"  # TODO Change this so all hosts can work
@@ -11,49 +11,63 @@ PORT = 8080
 SEGMENT_ID_SIZE = 6  # 6 bites for the segment ID according to subject
 SEGMENT_SIZE = 1500 - SEGMENT_ID_SIZE
 RTT = 0.005
+TIMEOUT = 0.01
+
+if "remote" in sys.argv:
+    import pydevd_pycharm
+
+    pydevd_pycharm.settrace('192.168.2.1', port=6969, stdoutToServer=True, stderrToServer=True)
 
 
 class BaseServer:
     def __init__(self) -> None:
         self.ServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # create UDP socket
         self.ServerSocket.bind((HOST, PORT))  # bind the socket to an address
-        logging.info("Socket binded with success")
 
         self.NewPort = random.randint(1000, 9999)
+        self.clientAddr = None
         self.clientPort = None
+
         self.DataSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # create UDP socket
         self.DataSocket.bind((HOST, self.NewPort))  # bind the socket to an address
+        logging.info("Socket binded with success")
 
         self.startTime = None
         self.endTime = None
         self.fileName = None
+        self.DroppedSegmentCount = 0
 
-        logging.info(f"Server listening at {PORT}")
+        logging.info(f"Server listening at {PORT} 🙉")
 
     def send(self, port, data):
         if type(data) == str:
-            self.ServerSocket.sendto(str.encode(data), (HOST, port))
+            self.ServerSocket.sendto(str.encode(data), (self.clientAddr, port))
         else:
-            self.ServerSocket.sendto(data, (HOST, port))
+            self.ServerSocket.sendto(data, (self.clientAddr, port))
 
     def rcv(self, Socket, bufferSize):
         return Socket.recvfrom(bufferSize)
 
+    def setTimeout(self):
+        self.ServerSocket.settimeout(TIMEOUT)
+        self.DataSocket.settimeout(TIMEOUT)
+
     def handshake(self):
         handshakeBuffer = 12
         message, address = self.rcv(self.ServerSocket, handshakeBuffer)
-        self.clientPort = address[1]
+        self.clientAddr, self.clientPort = address
         if b"SYN" in message:
-            logging.info(f"SYN Received from {address}")
+            logging.debug(f"SYN Received from {address}")
         else:
             raise ConnectionRefusedError
 
         self.send(self.clientPort, f"SYN-ACK{self.NewPort}")
-        logging.info(f"SYN-ACK sent 🚀")
+        logging.debug(f"SYN-ACK sent 🚀")
 
         message, address = self.rcv(self.ServerSocket, handshakeBuffer)
         if b"ACK" in message:
-            logging.info(f"Connection with {address} achieved")
+            logging.info(f"handshake with {address} achieved 🤝")
+            self.setTimeout()
         else:
             raise ConnectionRefusedError
 
@@ -65,12 +79,12 @@ class BaseServer:
     def ackHandler(self):
         # check for ACK
         rcvACK, _ = self.rcv(self.ServerSocket, 10)
-        logging.debug(f"received ACK from client: {rcvACK.decode()}")
+        # logging.debug(f"received ACK from client: {rcvACK.decode()}")
 
         return int(rcvACK.decode()[3:9])
 
     def writer(self, Segments):
-        logging.info("sending all segments")
+        # logging.info("sending all segments")
         for Segment in Segments:
             self.send(self.clientPort, Segment)
 
@@ -113,14 +127,16 @@ class BaseServer:
     def _postSendFile(self):
         self.send(self.clientPort, "FIN")
         self.endTime = time.time()
-        logging.info("File send with success 🎉")
+        logging.info(f"File {self.fileName} send with success 🎉")
         logging.info(f"Total time to send file {int((self.endTime - self.startTime) * 1000)} ms 🐢")
         rate = "{:.2f}".format(
             round(os.stat(self.fileName).st_size / int((self.endTime - self.startTime) * (10 ** 6)), 2))
+        logging.info(f"Number of dropped segments {self.DroppedSegmentCount}")
         logging.info(f"Transmission rate: {rate} MBps ")
 
     def engine(self, *args, **kwargs):
-        raise NotImplementedError
+        raise NotImplementedError("Base server can't be used on it's own, you need inherit it into your own subclass "
+                                  "and provide an engine to handle sending segments and receiving ACKs")
 
     def sendFile(self):
         segments = self._preSendFile()
@@ -128,3 +144,31 @@ class BaseServer:
         self.engine(segments)
 
         self._postSendFile()
+
+    def writeLogs(self, Name, Logs):
+        with open(Name + ".log", "w") as f:
+            for Log in Logs:
+                f.write(str(Log*1000) + "\n")
+        logging.debug(f"wrote ✍️  logs onto {Name}.log")
+
+    def checkFile(self):
+        FilePath1 = self.fileName
+        FilePath2 = f"../clients/copy_{self.fileName}"
+
+        with open(FilePath1, "rb") as f:
+            File1 = f.read()
+
+        with open(FilePath2, "rb") as f:
+            File2 = f.read()
+
+        if len(File1) != len(File2):
+            logging.warning(f"Different file sizes: original {len(File1)} copy: {len(File2)}")
+            logging.warning(f"missing bites {len(File1) - len(File2)}")
+
+        for index in range(len(File1)):
+            if File1[index] != File2[index]:
+                logging.warning(f"First difference at segment {index // 15000}")
+                break
+            if index == len(File1) - 1:
+                logging.info("No difference, files are identical 🥳")
+
